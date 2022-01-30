27ab6b484d4e282f70afff649599b6876de21f99
José María Beristain <dedocebabotas@hotmail.com>
2022-01-29 21:26:18 -0600

    actualización de animaciones de jugador y HUD con arte final

--- ggj2022/Assets/scripts/UI_player.cs
+++ ggj2022/Assets/scripts/UI_player.cs
@@ -5,8 +5,18 @@ using UnityEngine.UI;
 
 public class UI_player : MonoBehaviour
 {
+    [SerializeField] private Image face;
     [SerializeField] private Image hp_bar;
+    [SerializeField] private Image hp_background;
+    [SerializeField] private float hurt_danho;
     [SerializeField] private Image mp_bar;
+    [SerializeField] private Sprite angel_hp;
+    [SerializeField] private Sprite demonio_hp;
+    [SerializeField] private Sprite angel_face_normal;
+    [SerializeField] private Sprite demonio_face_normal;
+    [SerializeField] private Sprite angel_face_hurt;
+    [SerializeField] private Sprite demonio_face_hurt;
+
     private habilidades_jugador habilidades;
 
     // Start is called before the first frame update
@@ -22,5 +32,38 @@ public class UI_player : MonoBehaviour
     {
         hp_bar.fillAmount = habilidades.hp / habilidades.max_hp;
         mp_bar.fillAmount = habilidades.mp / habilidades.max_mp;
+
+        if (Preguntar_demonio())
+        {
+            hp_bar.sprite = demonio_hp;
+            hp_background.sprite = hp_bar.sprite;
+
+            if (habilidades.mp<=0)
+            {
+                face.sprite = demonio_face_hurt;
+            }
+            else
+            {
+                face.sprite = demonio_face_normal;
+            }
+
+        }
+        else {
+            hp_bar.sprite = angel_hp;
+            hp_background.sprite = hp_bar.sprite;
+
+            if (hp_bar.fillAmount >= hurt_danho)
+            {
+                face.sprite = angel_face_normal;
+            }
+            else {
+                face.sprite = angel_face_hurt;
+            }
+        }
+
+    }
+
+    bool Preguntar_demonio() {
+        return habilidades.demonio.modo_demonio;
     }
 }
--- ggj2022/Assets/scripts/angel-demonio/Movimiento_Jugador.cs
+++ ggj2022/Assets/scripts/angel-demonio/Movimiento_Jugador.cs
@@ -15,70 +15,180 @@ public class Movimiento_Jugador : MonoBehaviour
     private Rigidbody2D rigidbody2D;
     private Animator animator; 
     private float horizontal;
-    private bool Grounded;
-    private bool DoubleJump;
-
+    public bool DoubleJump;
+    public bool is_jumping;
+    public bool puede_aterrizar;
+    public bool Grounded;
 
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         habilidades = GetComponent<habilidades_jugador>();
+        animator.Play("idle_angel");
+        is_jumping = false;
     }
 
     void Update()
     {
-        if (habilidades.demonio.modo_demonio)
+        if (!habilidades.atacando)
         {
-            JumpForce = JumpforceDemon;
-            Speed = SpeedDemon;
-        }else
-        {
-            JumpForce = JumpForceAngel;
-            Speed = SpeedAngel;
-        }
-
-        horizontal = Input.GetAxisRaw("Horizontal");
-
-        if (horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
-        else if (horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-
-        animator.SetBool("Running", horizontal != 0.0f);
-
-        Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
-        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
-        {
-            Grounded = true;
-        }
-        else Grounded = false;
-
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            if (Grounded)
+            if (!habilidades.recibiendo_danho)
             {
-            Jump();
-            }else if (DoubleJump && !habilidades.demonio.modo_demonio)
+                if (habilidades.demonio.modo_demonio && !habilidades.demonio.berserker)
+                {
+                    JumpForce = JumpforceDemon;
+                    Speed = SpeedDemon;
+                }
+                else
+                {
+                    JumpForce = JumpForceAngel;
+                    Speed = SpeedAngel;
+                }
+
+                horizontal = Input.GetAxisRaw("Horizontal");
+
+                if (horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+                else if (horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+                if (!habilidades.demonio.modo_demonio)
+                {
+                    if (!is_jumping && Grounded)
+                    {
+                        if (horizontal == 0.0f)
+                        {
+                            animator.Play("idle_angel");
+                        }
+                        else
+                        {
+                            animator.Play("fly");
+                        }
+                    }
+                }
+                else
+                {
+                    if (!is_jumping && Grounded)
+                    {
+                        if (horizontal == 0.0f)
+                        {
+                            animator.Play("idle_demonio");
+                        }
+                        else
+                        {
+                            animator.Play("run_demonio");
+                        }
+                    }
+                }
+
+                if (is_jumping && Grounded && puede_aterrizar)
+                {
+                    if (habilidades.demonio.modo_demonio)
+                    {
+                        animator.Play("landing_demon");
+                    }
+                    else
+                    {
+                        animator.Play("idle_angel");//aterrizaje angel
+                    }
+                }
+
+
+                Debug.DrawRay(transform.position, Vector3.down * 0.125f, Color.red);
+                if (Physics2D.Raycast(transform.position, Vector3.down, 0.125f))
+                {
+                    Grounded = true;
+                }
+                else
+                {
+                    Grounded = false;
+                    puede_aterrizar = true;
+                    if (!is_jumping)
+                    {
+                        if (habilidades.demonio.modo_demonio)
+                        {
+                            animator.Play("falling_demon");
+                        }
+                        else
+                        {
+                            animator.Play("in_air_demon");// en aire angel
+                        }
+                    }
+                }
+
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    if (Grounded)
+                    {
+                        Jump();
+                    }
+                    else if (DoubleJump && (!habilidades.demonio.modo_demonio || habilidades.demonio.berserker))
+                    {
+                        rigidbody2D.AddForce(Vector2.zero);
+                        Jump();
+                        DoubleJump = false;
+                    }
+                }
+
+
+                if (Grounded)
+                    DoubleJump = true;
+            }
+            else
             {
-                rigidbody2D.AddForce(Vector2.zero);
-                Jump();
-                DoubleJump = false;
+                if (habilidades.demonio.modo_demonio)
+                {
+                    animator.Play("daño_demonio");
+                }
+                else
+                {
+                    animator.Play("daño_angel");
+                }
             }
         }
-
-
-        if (Grounded)
-            DoubleJump = true;
+        else {
+            if (habilidades.demonio.modo_demonio)
+            {
+                animator.Play("ataque_demonio");
+            }
+            else
+            {
+                animator.Play("ataque_demonio");
+            }
+        }
     }
 
     private void Jump()
     {
-            rigidbody2D.velocity = Vector2.zero;
-            rigidbody2D.AddForce(Vector2.up * JumpForce);
+        is_jumping = true;
+        puede_aterrizar = false;
+        if (habilidades.demonio.modo_demonio)
+        {
+            animator.Play("init_jump_demon");
+            print("jump demon");
+        }
+        else {
+            animator.Play("jump_angel");//salto angel
+        }
+    }
+
+    public void PhisicJump() {
+        rigidbody2D.velocity = Vector2.zero;
+        rigidbody2D.AddForce(Vector2.up * JumpForce);
+    }
+
+    public void EndJump() {
+        is_jumping = false;
+    }
+
+    public void PoderAterrizar() {
+        puede_aterrizar = true;
     }
 
 
     private void FixedUpdate()
     {
+        if(!habilidades.atacando)
+        if(!habilidades.recibiendo_danho)
         rigidbody2D.velocity = new Vector2(horizontal * Speed, rigidbody2D.velocity.y);
     }
 }
